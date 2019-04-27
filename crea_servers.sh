d5ca887bb7dc37c3cd923ebf701f435408f62611
Paolo <pamengames@gmail.com>
2019-04-28 00:14:58 +0200

    Create crea_servers.sh

---
+++ crea_servers.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+path_script=/opt/HP/BSM/opr/bin
+path_text_file=/home/user/Scripts_monitoraggio
+
+
+for SERVICE in $(cat $path_text_file/servizi.txt);do $path_script/opr-node.sh -list_nodes -view_name $SERVICE -username user -password passwd | grep Primary > servers_$SERVICE.txt
+
+for HOST in $(cat $path_text_file/servers_$SERVICE.txt | cut -d "=" -f2);do $path_text_file/test_restart_agent.sh $HOST; done
