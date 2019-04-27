bf6a3520859d64cd48ef620c9e82a538b2284f45
Paolo <pamengames@gmail.com>
2019-04-28 00:27:25 +0200

    Update and rename crea_servers.sh to lrestart_servers.sh

--- lrestart_servers.sh
+++ lrestart_servers.sh
@@ -1,3 +1,5 @@
+#Script per estrazione servers in base al servizio e restart di ogni agente 
+
 #!/bin/bash
 
 path_script=/opt/HP/BSM/opr/bin
@@ -6,4 +8,4 @@ path_text_file=/home/user/Scripts_monitoraggio
 
 for SERVICE in $(cat $path_text_file/servizi.txt);do $path_script/opr-node.sh -list_nodes -view_name $SERVICE -username user -password passwd | grep Primary > servers_$SERVICE.txt
 
-for HOST in $(cat $path_text_file/servers_$SERVICE.txt | cut -d "=" -f2);do $path_text_file/test_restart_agent.sh $HOST; done
+for HOST in $(cat $path_text_file/servers_$SERVICE.txt | cut -d "=" -f2);do $path_text_file/test_restart_agent_2.0.sh $HOST; done
